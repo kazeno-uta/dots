064116931f0e90ed037ce3e383e832bb96bb0941
Kazeno <max.soulie01@laposte.net>
2024-03-01 06:02:30 +0100

    fixed script not working

--- copy-conf.sh
+++ copy-conf.sh
@@ -1,4 +1,4 @@
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-cp -r ./* ~/
+cp -rf ./* ~/
+# curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
