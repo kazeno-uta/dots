57a0829edab69b6141871932c42a6edd2013a815
Kazeno <max.soulie01@laposte.net>
2024-06-08 20:48:57 +0000

    added desktop hyprland config

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    neofetch
-    hyprctl splash
-    set fish_greeting
-end
-zoxide init fish | source
-alias cd=z
-alias nv=nvim 
