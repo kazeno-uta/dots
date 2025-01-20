4a1d7bed4345aea142cfb0b7463e23bf0f3657d7
Maxime Soulie <soulie.e2302816@etud.univ-ubs.fr>
2025-01-20 15:34:46 +0100

    rework

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     uwufetch
-    hyprctl splash
+    hyprctl splash | cowsay -f tux
     set fish_greeting
 end
 zoxide init fish | source
@@ -12,3 +12,7 @@ alias BetterDiscord="~/Downloads/BetterDiscord-Linux.AppImage"
 alias LS=sl
 alias rtfm=man
 alias maxwell=" mpv -loop=inf --fs --mute 'https://www.youtube.com/watch?v=kOG0_qjKWEI'"
+alias mimir= "systemctl suspend"
+
+# Created by `pipx` on 2024-11-29 12:10:43
+set PATH $PATH /home/max/.local/bin
