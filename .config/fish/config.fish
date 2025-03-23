5db6c3741a927b6b777789526bf0093b06f5cfe2
Maxime Soulie <soulie.e2302816@etud.univ-ubs.fr>
2025-03-23 13:17:57 +0100

    can't rember

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,6 @@ alias nv=nvim
 zoxide init fish | source
 alias cd=z
 alias maxwell="mpv 'https://www.youtube.com/watch?v=kOG0_qjKWEI' --loop=inf --fs --mute"
+
+# Created by `pipx` on 2025-03-05 21:21:47
+set PATH $PATH /home/max/.local/bin
