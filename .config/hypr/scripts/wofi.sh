fd9c91f05f2b848c47b8415d4991b9bde911286d
Maxime Soulie <soulie.e2302816@etud.univ-ubs.fr>
2025-07-28 00:40:13 +0200

    updated wofi to look good on 1080 and 1440 monitors

--- .config/hypr/scripts/wofi.sh
+++ .config/hypr/scripts/wofi.sh
@@ -1,4 +1,29 @@
-if [pgrep wofi != null]; then
-	pkill wofi
+#!/bin/bash
+
+# Get clean cursor X and Y (remove any whitespace or newline)
+CUR_POS=$(hyprctl cursorpos)
+CUR_X=$(echo "$CUR_POS" | cut -d',' -f1 | tr -d '[:space:]')
+CUR_Y=$(echo "$CUR_POS" | cut -d',' -f2 | tr -d '[:space:]')
+
+# Get monitor data from hyprctl
+MONITOR_INFO=$(hyprctl monitors -j | jq -r --arg x "$CUR_X" --arg y "$CUR_Y" '
+  .[] | select(
+    (.x <= ($x | tonumber)) and 
+    (.x + .width > ($x | tonumber)) and 
+    (.y <= ($y | tonumber)) and 
+    (.y + .height > ($y | tonumber))
+  ) | "\(.name) \(.x) \(.y) \(.width) \(.height)"
+')
+
+# Parse values
+read -r MON_NAME MON_X MON_Y MON_W MON_H <<< "$MONITOR_INFO"
+
+# Optional: pick a different style per monitor
+if [[ "$MON_H" == 1440 ]]; then
+	STYLE="/home/max/dots/.config/wofi/style-1440.css"
+
+else
+	STYLE="/home/max/dots/.config/wofi/style-1080.css"
 fi
-wofi --show drun
+
+ wofi "$@" --style "$STYLE"
