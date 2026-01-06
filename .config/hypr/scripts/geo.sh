29e1f851a5c383b17f3658df5313944b8c9fed49
Maxime Soulie <soulie.e2302816@etud.univ-ubs.fr>
2026-01-06 15:48:46 +0100

    new windowrule syntax

--- .config/hypr/scripts/geo.sh
+++ .config/hypr/scripts/geo.sh
@@ -5,14 +5,14 @@ set windowId ""
 
 function handle
   switch $argv[1] 
-    case 'activewindow>>*GeoGuessr*changed=false*'
+    case 'activewindow>>zen*GeoGuessr*changed=false*'
       set changed true
       swww img -t wipe wallpaper/geoguessr_map_2025-2.png --transition-angle 200 -o HDMI-A-1 --transition-duration 0.9 --resize fit --fill-color 65A6E0FF
 
     case 'activewindowv2*changed=true*id='
       set windowId (string match -r '>>\w+' $argv[1] | string replace '>>' '')
 
-    case 'activewindow>>*changed=true*'
+    case 'activewindow>>zen*changed=true*'
       set hyprctlOut (string match -r 'Window \w+' (hyprctl activewindow) | string replace 'Window ' '')
       if test $hyprctlOut = $windowId -a (echo $argv[1] | grep -q "GeoGuessr"; echo $status) -ne 0
         swww img -t wipe wallpaper/crosscode2.gif --transition-angle 160 -o HDMI-A-1 --transition-duration 0.9
